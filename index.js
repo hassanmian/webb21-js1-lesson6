818974c7b9486fb83f9ce496ebf59dc11e65517f
“Hassan <mian@willandskill.se>
2021-09-22 13:16:42 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+class Animal {
+    constructor(name, legs) {
+        this.name = name
+        this.legs = legs
+    }
+    speech() {
+        return `${this.name} makes a noise`
+    }
+    numberOfLegs() {
+        return `This animal has ${this.legs} legs`
+    }
+}
+
+const dog = new Animal("Dog", 4)
+console.log( dog.speech() ) 
+console.log( dog.numberOfLegs() )
+
+const cat = new Animal("Cat", 4)
+console.log( cat.speech() )
+console.log( cat.numberOfLegs() )
+console.log( cat.name )
+
+const spider = new Animal("Spider", 8)
+console.log( spider.speech() )
+console.log( spider.numberOfLegs() )
